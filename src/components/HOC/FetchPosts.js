2ac2a7a5af871127bd9b1f99bf7a92d50e65c436
Emanuele Bertuol <emanuelebertuol@gmail.com>
2020-07-22 16:27:40 +0200

    a

--- src/components/HOC/FetchPosts.js
+++ src/components/HOC/FetchPosts.js
@@ -20,7 +20,6 @@ function postsData(WrappedComponent) {
     async fetchPosts() {
       let postsData = {
         method: "GET",
-        //url: `https://striveschool.herokuapp.com/api/posts/`,
         url: `http://localhost:3333/posts`,
         headers: {
           Authorization: "Basic " + btoa("user7:3UU5dYFvenRuRP7E"),
